5e081997c3512ecc9bf6910ca05d0d7239fd74bd
David Chivadze <datochicho11@gmail.com>
2023-12-11 04:53:08 +0400

    Commit message

---
+++ 03-01.test3.sql
@@ -0,0 +1,16 @@
+DAch^!39
+-- Insert some data into the table
+--INSERT INTO example_table2 VALUES (1, 'John');
+--INSERT INTO example_table VALUES (2, 'Jane');
+--INSERT INTO example_table VALUES (3, 'Bob');
+
+
+--insert into example_t123 values(1, 'test');
+
+select sysdate from dual;
+
+-- Commit the changes
+--COMMIT;
+
+-- Exit SQL*Plus
+EXIT COMMIT;
